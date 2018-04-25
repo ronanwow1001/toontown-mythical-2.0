0500fbf5f7c4c5b73bb0d8c1c3112c38c414eed5
ronanwow1001 <ronancaleb123@gmail.com>
2018-04-25 15:54:58 -0700

    Update start.command.bat

--- start.command.bat
+++ start.command.bat
@@ -1,7 +1,12 @@
+
 desktop nano 
 "demo executable"
+@echo off
+cd..
+win32/astron
 export TTR_GAMESERVER=54.174.138.210
 ppython -m launcher
 import "resources"
 from dirname "C:\Program Files x86\Toontown Rewritten"
 export "resources"
+pause
