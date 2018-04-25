6d0227459cf85bf76a0f7fa2df62d70cbc1fcc92
ronanwow1001 <ronancaleb123@gmail.com>
2018-04-25 11:40:43 -0700

    Create start.command.bat

---
+++ start.command.bat
@@ -0,0 +1,7 @@
+desktop nano 
+"demo executable"
+export TTR_GAMESERVER=54.174.138.210
+ppython -m launcher
+import "resources"
+from dirname "C:\Program Files x86\Toontown Rewritten.ink"
+export "resources"
