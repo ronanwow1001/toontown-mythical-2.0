24d66cdb1fb9f6e389065c3172c419eea1801156
ronanwow1001 <ronancaleb123@gmail.com>
2018-04-25 16:04:14 -0700

    Update start.command.bat

--- start.command.bat
+++ start.command.bat
@@ -3,10 +3,25 @@ desktop nano
 "demo executable"
 @echo off
 cd..
-win32/astron
+rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
+set /P PPYTHON_PATH=<PPYTHON_PATH
+
+rem Get the user input:
+set /P tteUsername="Username: "
+
+rem Export the environment variables:
+set ttrPassword=password
+set TTR_PLAYCOOKIE=%tteUsername%
+set TTR_GAMESERVER=127.0.0.1
+
+echo ===============================
+echo Starting Toontown Mythical...
+echo ppython: %PPYTHON_PATH%
+echo Username: %ttrUsername%
+echo Gameserver: %TTR_GAMESERVER%
+echo ===============================
 export TTR_GAMESERVER=54.174.138.210
 ppython -m launcher
 import "resources"
 from dirname "C:\Program Files x86\Toontown Rewritten"
 export "resources"
-pause
