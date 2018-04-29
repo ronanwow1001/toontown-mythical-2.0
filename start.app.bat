94048e71acaf144d2a107cd31324ce69482f5e20
ronanwow1001 <ronancaleb123@gmail.com>
2018-04-29 14:37:59 -0700

    test

--- start.app.bat
+++ start.app.bat
@@ -9,14 +9,13 @@ set /P ttmUsername="Username: "
 rem Export the environment variables:
 set ttrPassword=password
 set TTR_PLAYCOOKIE=%PPYTHON_PATH%
-set TTR_GAMESERVER=127.1.2.1
+set TTR_GAMESERVER=54.174.138.210
 echo ===============================
 echo Starting Toontown Mythical...
 echo ppython: %PPYTHON_PATH%
 echo Username: %ttrUsername%
 echo Gameserver: %TTR_GAMESERVER%
-echo ===============================
-export TTR_GAMESERVER=54.174.138.210
+echo =============================== 
 ppython -m launcher
 from dirname "C:\Program Files x86\Toontown Rewritten\"
 pause
